40005b3611fb440a6233599ac9ad97d3743038c5
Oleksandra-web2025-3 <kasyanyksasha@gmail.com>
2025-03-28 20:45:20 +0200

    task 2 added to index.js

--- index.js
+++ index.js
@@ -3,53 +3,50 @@ const fs = require('fs');
 const path = require('path');
 
 const program = new Command();
-
 program
-  .option('-i, --input <path>', 'Path to input file')
-  .option('-o, --output <path>', 'Path to output file')
-  .option('-d, --display', 'Display result in console');
-
+  .option('-i, --input <path>', 'input file')
+  .option('-o, --output <path>', 'output file')
+  .option('-d, --display', 'show in console');
 program.parse(process.argv);
 
 const options = program.opts();
 const inputPath = options.input;
 const outputPath = options.output;
-const display = options.display;
+const showDisplay = options.display;
 
 if (!inputPath) {
-  console.error('Please, specify input file');
+  console.log('Вкажіть файл для обробки');
   process.exit(1);
 }
 
 if (!fs.existsSync(inputPath)) {
-  console.error('Cannot find input file');
+  console.log('Файл не знайдено');
   process.exit(1);
 }
 
-fs.readFile(inputPath, 'utf8', (err, data) => {
-  if (err) {
-    console.error('Error reading input file:', err.message);
+try {
+  const fileData = fs.readFileSync(inputPath, 'utf8');
+  const jsonData = JSON.parse(fileData);
+  
+  const targetCategories = ['Доходи, усього', 'Витрати, усього'];
+  const result = targetCategories
+    .filter(category => jsonData[category] && jsonData[category].value !== undefined)
+    .map(category => `${category}:${jsonData[category].value}`)
+    .join('\n');
+  
+  if (!result) {
+    console.log('Не знайдено потрібних категорій');
     process.exit(1);
   }
-
-  try {
-    const parsedData = JSON.parse(data);
-    const result = JSON.stringify(parsedData, null, 2);
-
-    if (!outputPath && !display) {
-      process.exit(0);
-    }
-
-    if (outputPath) {
-      fs.writeFileSync(outputPath, result, 'utf8');
-    }
-
-    if (display) {
-      console.log(result);
-    }
-  } catch (parseError) {
-    console.error('Error parsing JSON:', parseError.message);
-    process.exit(1);
+  
+  if (outputPath) {
+    fs.writeFileSync(outputPath, result, 'utf8');
   }
-});
-
+  
+  if (showDisplay) {
+    console.log(result);
+  }
+} catch (error) {
+  console.log('Помилка опрацювання файлу');
+  process.exit(1);
+}
