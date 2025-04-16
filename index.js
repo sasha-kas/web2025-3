8ad63095b0d21c59a796391242ceea6941f6c932
Oleksandra-web2025-3 <kasyanyksasha@gmail.com>
2025-04-16 23:40:52 +0300

    ура нарешті кінцева версія коду

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const { Command } = require('commander');
 const fs = require('fs');
-const path = require('path');
 
 const program = new Command();
 program
-  .option('-i, --input <path>', 'input file')
-  .option('-o, --output <path>', 'output file')
-  .option('-d, --display', 'show in console');
+  .option('-i, --input <path>', 'input file (обовʼязковий параметр)')
+  .option('-o, --output <path>', 'output file (необовʼязковий параметр)')
+  .option('-d, --display', 'show in console (необовʼязковий параметр)');
 program.parse(process.argv);
 
 const options = program.opts();
@@ -15,38 +14,47 @@ const outputPath = options.output;
 const showDisplay = options.display;
 
 if (!inputPath) {
-  console.log('Вкажіть файл для обробки');
+  console.log('Please, specify input file');
   process.exit(1);
 }
 
 if (!fs.existsSync(inputPath)) {
-  console.log('Файл не знайдено');
+  console.log('Cannot find input file');
   process.exit(1);
 }
 
 try {
   const fileData = fs.readFileSync(inputPath, 'utf8');
   const jsonData = JSON.parse(fileData);
-  
+
+  // Фільтруємо потрібні категорії з масиву
   const targetCategories = ['Доходи, усього', 'Витрати, усього'];
-  const result = targetCategories
-    .filter(category => jsonData[category] && jsonData[category].value !== undefined)
-    .map(category => `${category}:${jsonData[category].value}`)
-    .join('\n');
-  
+  let result = '';
+
+  jsonData.forEach(item => {
+    if (item.txt && targetCategories.includes(item.txt)) {
+      result += `${item.txt}:${item.value}\n`;
+    }
+  });
+
+  // Якщо результат порожній
   if (!result) {
     console.log('Не знайдено потрібних категорій');
     process.exit(1);
   }
-  
+
+  // Записуємо в файл, якщо задано -o
   if (outputPath) {
     fs.writeFileSync(outputPath, result, 'utf8');
   }
-  
+
+  // Виводимо в консоль, якщо задано -d
   if (showDisplay) {
     console.log(result);
   }
+
 } catch (error) {
   console.log('Помилка опрацювання файлу');
   process.exit(1);
 }
+
